0d26e2da7576b91c537262f77a95d1a8b01aaeee
Gregory <insmartdatascience@gmail.com>
2023-07-11 22:19:40 +0300

    added more Codewars exercises

--- Codewars/ex_5_symbol_occurrences.py
+++ Codewars/ex_5_symbol_occurrences.py
@@ -2,6 +2,7 @@
 # and returns an integer of the count of occurrences the 2nd argument is found in the first one
 # If no occurrences can be found, a count of 0 should be returned
 
+
 def str_count(strng, letter):
     if letter in strng:
         return strng.count(letter)
