4852e4b8a2c86b648e680fba90f249045c3f6e41
Gregory <insmartdatascience@gmail.com>
2023-07-14 23:17:35 +0300

    minor docstring updates

--- Practice/geometry_functions.py
+++ Practice/geometry_functions.py
@@ -1,5 +1,6 @@
 def line_segment_length(point_1: int or float, point_2: int or float):
-    """
+    """ Given two points, calculates the length of a line segment.
+
     :param point_1: Point 1 of the line segment
     :param point_2: Point 2 of the line segment
     :type point_1: int or float
@@ -14,7 +15,9 @@ def line_segment_length(point_1: int or float, point_2: int or float):
 
 
 def line_slope(x1: int or float, y1: int or float, x2: int or float, y2: int or float):
-    """
+    """ Given coordinates of two points (on 'x' and 'y' axes),
+    the function calculates the slope of the line between those points.
+
     :param x1: Point 1 coordinate on the 'x' axis
     :param y1: Point 1 coordinate on the 'y' axis
     :param x2: Point 2 coordinate on the 'x' axis
@@ -33,7 +36,9 @@ def line_slope(x1: int or float, y1: int or float, x2: int or float, y2: int or 
 
 
 def line_midpoint(x1: int or float, y1: int or float, x2: int or float, y2: int or float):
-    """
+    """ Given coordinates of two points (on 'x' and 'y' axes),
+    the function calculates the middle point of the line between those points.
+
     :param x1: Point 1 coordinate on the 'x' axis
     :param y1: Point 1 coordinate on the 'y' axis
     :param x2: Point 2 coordinate on the 'x' axis
