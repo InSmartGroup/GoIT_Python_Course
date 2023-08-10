08dff297760f21a3040e78d28a07e28a55fad79f
Gregory <insmartdatascience@gmail.com>
2023-08-10 22:02:42 +0300

    minor homework_1 updates

--- Homeworks/homework_1/main.py
+++ Homeworks/homework_1/main.py
@@ -1,25 +1,14 @@
 import sys
 from pathlib import Path
 import shutil
-import functions
+import files_generator, transliteration
+
+path = Path.cwd()/'various_files'
+print(path)
 
 # this function was used to automatically create multiple files, please ignore this piece of code
-# functions.create_empty_files(path, name_prefix='', name_suffix="_old")
-
-# path = "C:\\Users\\gostapenko\\PycharmProjects\\GoIT_Python_Course\\Homeworks\\homework_1\\various_files\\"
-
-# standard pre-check
-try:
-    user_path = Path(sys.argv[0])
-    print(user_path)
-except IndexError:
-    print("Please specify the folder path.")
-    sys.exit()
-if not user_path.exists():
-    print("Invalid path. Please try again.")
-    sys.exit()
-
-
+# path = r"C:\Users\gostapenko\PycharmProjects\GoIT_Python_Course\Homeworks\homework_1\various_files"
+# files_generator.create_empty_files(path, name_prefix='', name_suffix="_old")
 
 if __name__ == "__main__":
     pass
