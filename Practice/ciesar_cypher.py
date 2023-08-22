91e3e9d5ebc9ce65b5175ffdee0a96f5efe61c89
Gregory <insmartdatascience@gmail.com>
2023-08-22 14:10:49 +0300

    adjusted the Ciesar Cypher code

--- Practice/ciesar_cypher.py
+++ Practice/ciesar_cypher.py
@@ -1,3 +1,6 @@
+import pathlib
+
+
 def encrypt(text, shift=None):
     coded_text = ""
     text_b = bytearray(text.encode('utf-8'))
@@ -29,11 +32,20 @@ def decrypt(text_bytes, shift):
     return text_bytes, decoded_text
 
 
+path = pathlib.Path.cwd()
+print(path)
+
 to_encrypt = input("Type something: ")
-key = 33
+key = 5
 
 coded_bytes, coded = encrypt(to_encrypt, key)
 decoded_bytes, decoded = decrypt(coded_bytes, key)
 
 print(f"Encrypted message: {coded}")
 print(f"Decrypted message: {decoded}")
+
+with open(path/'password.txt', 'w') as file:
+    file.write("Encrypted Password\n")
+    file.write(f"{''.center(20, '_')}\n")
+    file.write(f"{coded.center(20)}\n")
+    file.write(f"{''.center(20, '_')}\n")
