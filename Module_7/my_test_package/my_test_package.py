249d708a4a25b0267e6838ed9b734127a85a49bc
Gregory <insmartdatascience@gmail.com>
2023-08-29 18:31:58 +0300

    minor adjustments

---
+++ Module_7/my_test_package/my_test_package.py
@@ -0,0 +1,6 @@
+def func():
+    print('TEST')
+
+
+def func2():
+    return '2 + 2 = 4'
---
+++ Module_7/test.py
@@ -0,0 +1,15 @@
+# import os
+# from my_test_package import my_test_package as mtp
+#
+#
+# path = os.getcwd()
+# mtp.func()
+#
+# result = mtp.func2()
+# print(result)
+
+# cyrillic = {'а': 'a', "б": 'b', "в": 'v'}
+# translation_table = str.maketrans(cyrillic)
+#
+# translated_text = 'абв'.translate(translation_table)
+# print(translated_text)
