829fd5e1a315364e5d02beee1d72ba1a75119707
Gregory <insmartdatascience@gmail.com>
2023-09-01 00:01:14 +0300

    added datetime_module.py file

---
+++ Module_8/datetime_module.py
@@ -0,0 +1,29 @@
+import datetime
+from datetime import datetime as dt
+
+date_now = dt.now()
+# print(date_now.date())
+# print(date_now.strftime('%D'))  # common date format
+# print(date_now.strftime('%A'))  # day, full
+# print(date_now.strftime('%a'))  # day, short
+# print(date_now.strftime('%d'))  # day, digit
+# print(date_now.strftime('%Y'))  # year
+# print(date_now.strftime('%B'))  # month, full
+# print(date_now.strftime('%b'))  # month, short
+# print(date_now.strftime('%m'))  # month, digit
+# print(date_now.strftime('%H'))  # hour
+# print(date_now.strftime('%M'))  # minute
+# print(date_now.strftime('%S'))  # second
+
+# date_ts = dt(year=1986, month=11, day=15, hour=23, minute=17, second=41)
+# print(date_ts.strftime('%Y %B, %A %dth, %H:%M:%S'))  # custom datetime object formatting
+# print(dt.strptime('1986 November, Saturday 15th, 23:17:41', '%Y %B, %A %dth, %H:%M:%S'))  # from string to datetime
+# print(date_ts.timestamp())  # datetime object to timestamp
+# print(dt.fromtimestamp(532473461.0))  # from timestamp to datetime object
+
+date_bd = dt(year=2013, month=5, day=24, hour=19, minute=30)
+print(date_bd.strftime("My daughter was born in %Y, %b %dth at %H:%M. It was %A."))
+difference_days = date_now.date() - date_bd.date()
+print(f"It was {difference_days.days} days ago. I remember that day very well.")
+difference_years = date_now.year - date_bd.year
+print(f"Now she is {difference_years} years old. I love her so much!")
