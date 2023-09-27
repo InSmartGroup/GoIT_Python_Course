09ec6dfdcf665988834748b976533676a05b6923
Gregory <insmartdatascience@gmail.com>
2023-09-27 10:09:03 +0300

    added homework 6

--- Module_10/oop_light_switch.py
+++ Module_10/oop_light_switch.py
@@ -37,3 +37,59 @@
 # print(light_switch.switch_is_on)
 # light_switch.turn_off()
 # print(light_switch.switch_is_on)
+
+
+# class LightSwitch:
+#     def __init__(self):
+#         self.turned_on = False
+#         self.brightness_level = 0
+#
+#     def turn_on(self):
+#         self.turned_on = True
+#
+#     def turn_off(self):
+#         self.turned_on = False
+#
+#     def increase_brightness(self, amount):
+#         if self.turned_on:
+#             self.brightness_level += amount
+#
+#         if self.brightness_level > 100:
+#             self.brightness_level = 100
+#
+#         print(f"Brightness increased to {self.brightness_level}")
+#
+#     def decrease_brightness(self, amount):
+#         if self.turned_on:
+#             self.brightness_level -= amount
+#
+#         if self.brightness_level < 0:
+#             self.brightness_level = 0
+#
+#         print(f"Brightness decreased to {self.brightness_level}")
+#
+#     def report(self):
+#         print("REPORT".center(25, "-"))
+#         if self.turned_on:
+#             print("Light switch is on.")
+#             print(f"Brightness level: {self.brightness_level}")
+#         else:
+#             print("Light switch is off.")
+#             print(f"Brightness level: {self.brightness_level}")
+#         print("".center(25, "_"))
+#
+#
+# light_switch = LightSwitch()
+# light_switch.increase_brightness(10)  # won't work
+# light_switch.turn_on()
+# light_switch.increase_brightness(10)
+# light_switch.report()
+# light_switch.turn_off()
+# light_switch.report()
+# light_switch.increase_brightness(50)
+# light_switch.report()
+# light_switch.turn_on()
+# light_switch.increase_brightness(50)
+# light_switch.report()
+# light_switch.decrease_brightness(200)
+# light_switch.report()
--- Module_10/test.py
+++ Module_10/test.py
@@ -22,6 +22,90 @@
 # cat.change_age(5)
 # print(cat.get_info())
 
-class Character:
-    def __init__(self):
-        pass
+
+# class Character:
+#     hp = 100
+#     mp = 100
+#
+#     def __init__(self, name):
+#         self.name = name
+#         self.right_hand = None
+#         self.left_hand = None
+#
+#     def pick_weapon(self, weapon):
+#         if self.right_hand is None:
+#             self.right_hand = weapon
+#         elif self.left_hand is None:
+#             self.left_hand = weapon
+#         else:
+#             print("I can't pick any more weapons.")
+#
+#     def show_weapon(self):
+#         print(self.right_hand, self.left_hand)
+#
+#     def move(self):
+#         print("I'm moving.")
+#
+#     def attack(self):
+#         if self.right_hand is None and self.left_hand is None:
+#             print('I have no weapon.')
+#         else:
+#             print("I'm attacking.")
+#
+#     def defend(self):
+#         if self.right_hand is None and self.left_hand is None:
+#             print("I have no shield.")
+#         else:
+#             print("I'm defending.")
+#
+#
+# class Weapon:
+#     def __init__(self, type):
+#         self.type = type
+#         self.damage = 15
+#
+#
+# char = Character('Bro')
+# sword = Weapon('sword')
+# shield = Weapon('shield')
+#
+# char.pick_weapon(sword)
+# char.show_weapon()
+# char.attack()
+#
+# char.pick_weapon(shield)
+# char.show_weapon()
+# char.defend()
+
+
+# class Weapon:
+#     def __init__(self):
+#         self.damage = None
+#
+#     def hit(self):
+#         return self.damage
+#
+#     def throw_weapon(self):
+#         return self
+#
+#
+# class Sword(Weapon):
+#     def __init__(self):
+#         super().__init__()
+#         self.damage = 15
+#
+#
+# class Axe(Weapon):
+#     def __init__(self):
+#         super().__init__()
+#         self.damage = 20
+#
+#
+# sword = Sword()
+# axe = Axe()
+#
+# print(sword.hit())
+# print(axe.hit())
+# print(sword.throw_weapon())
+# print(axe.throw_weapon())
+
